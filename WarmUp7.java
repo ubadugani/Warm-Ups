c416c3ed2ef4e7668413c1fffbadd54ede0e382a
ubadugani <80549415+ubadugani@users.noreply.github.com>
2021-03-16 13:43:24 -0500

    Update WarmUp7.java

--- WarmUp7.java
+++ WarmUp7.java
@@ -9,49 +9,40 @@ public class WarmUp7 {
         month = scan.nextInt();
         System.out.println("Enter day: ");
         day = scan.nextInt();
-        if(month<=3 ||month== 12){
-            if(month==12 && day<16){
+        if(month <= 3 || month == 12){
+            if(month == 12 && day < 16){
                 System.out.println("It is Fall");
             }
-            if(month==3&&day>15){
+            if(month == 3 && day > 15){
                 System.out.println("It is Spring");
             }
-            /*else{
-                System.out.println("It is Winter");
-            }*/
         }
-        if(month<=6 &&month>=3){
-            if(month==3 && day<16){
+        if(month <= 6 && month >= 3){
+            if(month == 3 && day < 16){
                 System.out.println("It is Winter");
-            }
-            if(month==6&&day>15){
+            } 
+            if(month == 6 && day > 15){
                 System.out.println("It is Summer");
             }
-            /*else{
-                System.out.println("It is Spring");
-            }*/
+            
         }
-        if(month<=9 &&month>=6){
-            if(month==6 && day<16){
+        if(month <= 9 && month >= 6){
+            if(month == 6 && day < 16){
                 System.out.println("It is Spring");
             }
-            if(month==9&&day>15){
+            if(month == 9 && day > 15){
                 System.out.println("It is Fall");
             }
-            /*else{
-                System.out.println("It is Summer");
-            }*/
+            
         }
-        if(month<=12 &&month>=9){
-            if(month==9 && day<16){
+        if(month <= 12 && month >= 9){
+            if(month == 9 && day < 16){
                 System.out.println("It is Summer");
             }
-            if(month==12&&day>15){
+            if(month == 12 && day > 15){
                 System.out.println("It is Winter");
             }
-            /*else{
-                System.out.println("It is Fall");
-            }*/
+            
         }
 
 
